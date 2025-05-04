81efe963fba26b2367d1af99cae63f145a9872dd
AlessandroADP <tu_correo_electronico@example.com>
2025-05-03 23:22:54 -0500

    agregacion de admin

--- Ponte_Ricoso/personajes/admin.py
+++ Ponte_Ricoso/personajes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import personajes  
 
 # Register your models here.
+class personajesAdmin(admin.ModelAdmin):
+    fields = ['nombre', 'estado', 'especies', 'genero', 'origen', 'ubicacion', 'imagen', 'URL']
+    list_display = ['nombre', 'estado', 'especies', 'genero', 'origen', 'ubicacion']
+
+admin.site.register(personajes, personajesAdmin)
--- Ponte_Ricoso/personajes/models.py
+++ Ponte_Ricoso/personajes/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
 class personajes(models.Model):
-    nombre = models.CharField(max_length=255)
-    estado = models.CharField(max_length=50, choices=[('Alive', 'Vivo'), ('Dead', 'Muerto'), ('unknown', 'Desconocido')])
-    especies = models.CharField(max_length=100)
-    genero = models.CharField(max_length=50, choices=[('Female', 'Mujer'), ('Male', 'Hombre'), ('Genderless', 'Sin género'), ('unknown', 'Desconocido')])
-    origen = models.CharField(max_length=255, blank=True, null=True)
-    ubicacion = models.CharField(max_length=200,blank=True, null=True)
-    imagen = models.URLField()
-    URL = models.URLField(blank=True, null=True)
-    created = models.DateTimeField(auto_now_add=True)
+    nombre = models.CharField(max_length=255, verbose_name='Nombre')
+    estado = models.CharField(max_length=50, verbose_name="Estado" , choices=[('Alive', 'Vivo'), ('Dead', 'Muerto'), ('unknown', 'Desconocido',)])
+    especies = models.CharField(max_length=100, verbose_name="Especie")
+    genero = models.CharField(max_length=50, verbose_name="Genero",choices=[('Female', 'Mujer'), ('Male', 'Hombre'), ('Genderless', 'Sin género'), ('unknown', 'Desconocido')])
+    origen = models.CharField(max_length=255, blank=True, null=True,verbose_name="Origen")
+    ubicacion = models.CharField(max_length=200,blank=True, null=True, verbose_name="Ubicación")
+    imagen = models.URLField(verbose_name="Imagen")
+    URL = models.URLField(blank=True, null=True,verbose_name="URL")
+    created = models.DateTimeField(auto_now_add=True,verbose_name="Creado")
+
+    class Meta:
+        db_table = 'Rick y Morty'
+        verbose_name = "Personaje"
+        verbose_name_plural = "Personajes"
+        ordering = ['-created']
+
 
     def __str__(self):
-        return self.nombre
+        return self.nombre  
+    
